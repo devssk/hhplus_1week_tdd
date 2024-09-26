cf45ae74a502abcd0b9b3a2c68329f2c1671b689
devssk <sksong419@naver.com>
2024-09-26 12:51:35 +0900

    test: 포인트 충전 기능 테스트 코드 개발

---
+++ src/test/java/io/hhplus/tdd/point/ChargeTest.java
@@ -0,0 +1,49 @@
+package io.hhplus.tdd.point;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ChargeTest {
+
+    @InjectMocks
+    PointService pointService;
+
+    @Mock
+    PointHistoryRepository pointHistoryRepository;
+
+    @Mock
+    UserPointRepository userPointRepository;
+
+    @Mock
+    PointServiceValidation pointServiceValidation;
+
+    @Test
+    @DisplayName("정상 충전")
+    void chargeTest01() {
+        // given
+        long id = 1L;
+        long amount = 1000L;
+
+        doReturn(new UserPoint(id, 0, System.currentTimeMillis())).when(userPointRepository).selectById(anyLong());
+        doReturn(new UserPoint(id, amount, System.currentTimeMillis())).when(userPointRepository).insertOrUpdate(anyLong(), anyLong());
+
+        // when
+        UserPoint result = pointService.charge(id, amount);
+
+        // then
+        assertAll(() -> {
+            assertEquals(id, result.id());
+            assertEquals(amount, result.point());
+        });
+    }
+
+}
